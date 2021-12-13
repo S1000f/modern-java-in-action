970ba0020a96964a2a56b12b5e4354109a6b4f21
Nick <dohp.kim@gmail.com>
2021-12-13 22:30:10 +0900

    Stream, custom Collector

---
+++ src/main/java/test/stream/ToListCollector.java
@@ -0,0 +1,44 @@
+package test.stream;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
+
+  // 빈 컨테이너 만들기
+  @Override
+  public Supplier<List<T>> supplier() {
+    return ArrayList::new;
+  }
+
+  @Override
+  public BiConsumer<List<T>, T> accumulator() {
+    return List::add;
+  }
+
+  @Override
+  public BinaryOperator<List<T>> combiner() {
+    return (list1, list2) -> {
+      list1.addAll(list2);
+      return list1;
+    };
+  }
+
+  @Override
+  public Function<List<T>, List<T>> finisher() {
+    return Function.identity();
+  }
+
+  @Override
+  public Set<Characteristics> characteristics() {
+    return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH, Characteristics.CONCURRENT));
+  }
+}
